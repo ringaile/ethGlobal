1e347d9bf2dc67d513c63d6ab1f5ab9574584a8f
Ringaile Valiaugaite <ringaile@gmail.com>
2021-10-01 22:07:49 +0200

    getting token holders

---
+++ index.js
@@ -0,0 +1,6 @@
+const axios = require('axios')
+
+axios.get('https://kovan-api.ethplorer.io/getTopTokenHolders/0x47d4fe599eec755f055d54848d6ff12f2bb8a41a?apiKey=').then(resp => {
+
+    console.log(resp.data);
+});
